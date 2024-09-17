6ffa5cb86f20eb4d8a7a7bd75e87dd01ec76a188
NO-MM <khuzwanohnoh@gmail.com>
2024-09-16 22:06:29 -0700

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string[:]:
-        new_string = my_string.translate({ord("C"): None})
-        second_string = new_string.translate({ord("C"): None})
+        new_string = my_string.translate({ord("C"): None, ord("c"): None})
         return new_string
