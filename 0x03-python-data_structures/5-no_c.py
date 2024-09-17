4711fd4fff9d37242784928ed412217c06071c58
NO-MM <khuzwanohnoh@gmail.com>
2024-09-16 22:08:12 -0700

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string[:]:
+    if my_string:
         new_string = my_string.translate({ord("C"): None, ord("c"): None})
         return new_string
