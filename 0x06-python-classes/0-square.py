e3dbb57d5ba25f8678610d4134308c224f583e74
NO-MM <khuzwanohnoh@gmail.com>
2024-10-02 04:49:18 -0700

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Define a Square class"""
+"""Define a square class"""
 class Square:
-    """Representation of a Square class"""
+    """Representation of a square class"""
     pass
