849316d7631cd8fde23e66c782c6a4fe14774ab8
NO-MM <khuzwanohnoh@gmail.com>
2024-10-18 01:44:42 -0700

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining write_file with two arguments."""
+
+
+def write_file(filename="", text=""):
+    """Writes text to a file using UTF-8 encoding."""
+    with open(filename, "w", encoding='utf-8') as f:
+        return f.write(text)
+
