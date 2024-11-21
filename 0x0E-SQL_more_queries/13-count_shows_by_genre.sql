cd83264898b8826beb3d4728cfca09765c2f0f03
NO-MM <khuzwanohnoh@gmail.com>
2024-11-20 21:18:44 -0800

    Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,3 @@ SELECT tv_genres.name, As genre, COUNT(show_id) As number_of_shows
 FROM tv_genres JOIN tv_show_genres ON tv_genre.id = tv_show_genres.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
-);
